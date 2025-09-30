1e964e1f07d3e675cb91c77f5c4b28e29b099cc1
JC <168548230+JChung-Research@users.noreply.github.com>
2025-09-30 14:19:51 -0400

    Initialize BOPTEST test cases with the current timestamp
    Ensure that each BOPTEST test case is initialized at the current time to maintain consistent time zones and accurate timestamps.

--- volttron/client/control_init.py
+++ volttron/client/control_init.py
@@ -2,6 +2,7 @@
 import requests, json
 import logging
 from volttron.platform.agent import utils
+from datetime import datetime, timezone
 
 utils.setup_logging()
 _log = logging.getLogger(__name__)
@@ -9,6 +10,9 @@ _log = logging.getLogger(__name__)
 # BASE = 'http://host.docker.internal:5000'  # or your hosted API endpoint
 BASE = 'http://api.boptest.net' # LBNL's BOPTEST API endpoint
 HEADERS = {'Content-Type': 'application/json'}
+
+now = datetime.now(timezone.utc)
+current_timestep = now.hour*3600 + now.minute*60 + now.second
 
 step = 300
 scenario_period = 'peak_heat_day'
@@ -18,8 +22,8 @@ def initialize(testcase):
     _log.info('[INFO] Select testcase: {}'.format(testcase))
 
     testid = requests.post('{}/testcases/{}/select'.format(BASE,testcase)).json()['testid'] # testcase info should be imported from config files
-    
-    init_result = requests.put('{}/initialize/{}'.format(BASE, testid), json={'start_time': 1*24*3600,
+
+    init_result = requests.put('{}/initialize/{}'.format(BASE, testid), json={'start_time': 1*24*3600 + current_timestep,
                                                                               'warmup_period': 1*24*3600}).json()['message']
     _log.info('[INFO] testid "{}": {}'.format(testid, init_result))
     
